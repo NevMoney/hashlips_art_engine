90222c540f8165e326556031d85b0e1617636be1
Nev <67838965+NevMoney@users.noreply.github.com>
2022-03-06 22:10:24 -0500

    changes some files

--- nft_collection/smart-contract/config/CollectionConfig.ts
+++ nft_collection/smart-contract/config/CollectionConfig.ts
@@ -9,24 +9,24 @@ const CollectionConfig: CollectionConfigInterface = {
   // The contract name can be updated using the following command:
   // yarn rename-contract NEW_CONTRACT_NAME
   // Please DO NOT change it manually!
-  contractName: 'YourNftToken',
-  tokenName: 'My NFT Token',
-  tokenSymbol: 'MNT',
-  hiddenMetadataUri: 'ipfs://__CID__/hidden.json',
+  contractName: 'IntelligentInvestors',
+  tokenName: 'Intelligent Investors',
+  tokenSymbol: 'II',
+  hiddenMetadataUri: 'ipfs://QmazZAV5xpUb24PZsqXWSg4JAdS4kaNiVmt58GmaQXLvdp/hidden.json',
   maxSupply: 10000,
   whitelistSale: {
-    price: 0.05,
+    price: 0.02,
     maxMintAmountPerTx: 1,
   },
   preSale: {
-    price: 0.07,
+    price: 0.05,
     maxMintAmountPerTx: 2,
   },
   publicSale: {
-    price: 0.09,
+    price: 0.08,
     maxMintAmountPerTx: 5,
   },
-  contractAddress: null,
+  contractAddress: '0xA444412E110048F033E5dfD301B0f59ED02AFC2f',
   marketplaceIdentifier: 'my-nft-token',
   marketplaceConfig: openSea,
   whitelistAddresses: whitelistAddresses,
